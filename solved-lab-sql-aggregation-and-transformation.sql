63b39ca08d47470abe309bc1002b11e237dde4bd
Ana Cañizares <canizares.alvarez.ana@gmail.com>
2025-05-02 15:53:59 +0200

    solved-lab

---
+++ solved-lab-sql-aggregation-and-transformation.sql
@@ -0,0 +1,58 @@
+# 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
+SELECT 
+    FLOOR(MIN(length)) AS min_duration,
+    ROUND(MAX(length), 0) AS max_duration
+FROM film;
+
+# 2. You need to gain insights related to rental dates: 2.1 Calculate the number of days that the company has been operating.
+SELECT DATEDIFF(
+    (SELECT MAX(rental_date) FROM rental),
+    (SELECT MIN(rental_date) FROM rental)
+) AS days_operating;
+
+# 2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results.
+SELECT rental_id, rental_date, inventory_id, customer_id, return_date, staff_id, last_update,
+       MONTH(rental_date) AS rental_month,
+       DAYNAME(rental_date) AS rental_weekday
+FROM rental
+LIMIT 20;
+
+# 2.3 Bonus: Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week.
+# Hint: use a conditional expression.
+# NOT DONE.
+
+# 3. You need to ensure that customers can easily access information about the movie collection. To achieve this, retrieve the film titles and their rental duration. If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
+SELECT title, IFNULL(rental_duration, 'Not Available') AS rental_duration
+FROM film
+ORDER BY title ASC;
+
+# 4. Bonus: The marketing team for the movie rental company now needs to create a personalized email campaign for customers. To achieve this, you need to retrieve the concatenated first and last names of customers, along with the first 3 characters of their email address, so that you can address them by their first name and use their email address to send personalized recommendations. The results should be ordered by last name in ascending order to make it easier to use the data.
+# NOT DONE
+
+# Challenge 2. 1. Next, you need to analyze the films in the collection to gain some more insights. Using the film table, determine:
+# 1.1 The total number of films that have been released.
+SELECT COUNT(*) AS total_films_released FROM film;
+
+# 1.2 The number of films for each rating.
+SELECT rating, COUNT(*) AS film_count 
+FROM film 
+GROUP BY rating;
+
+# 1.3 The number of films for each rating, sorting the results in descending order of the number of films. This will help you to better understand the popularity of different film ratings and adjust purchasing decisions accordingly.
+SELECT rating, COUNT(*) AS film_count 
+FROM film 
+GROUP BY rating 
+ORDER BY film_count DESC;
+
+# 2. Using the film table, determine:
+# 2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
+SELECT rating, ROUND(AVG(length), 2) AS avg_duration 
+FROM film 
+GROUP BY rating 
+ORDER BY avg_duration DESC;
+
+# 2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers who prefer longer movies.
+SELECT rating, ROUND(AVG(length), 2) AS avg_duration 
+FROM film 
+GROUP BY rating 
+HAVING avg_duration > 120;
