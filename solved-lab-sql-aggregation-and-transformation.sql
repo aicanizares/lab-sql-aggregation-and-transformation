71740d17f5d6e6649600aba41f6688f5d0aaf563
Ana Cañizares <canizares.alvarez.ana@gmail.com>
2025-05-05 10:08:23 +0200

    solved-again

--- solved-lab-sql-aggregation-and-transformation.sql
+++ solved-lab-sql-aggregation-and-transformation.sql
@@ -26,7 +26,14 @@ LIMIT 20;
 
 # 2.3 Bonus: Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week.
 # Hint: use a conditional expression.
-# NOT DONE.
+# Monday is weekday O.
+SELECT 
+    rental_id, rental_date,
+    CASE 
+        WHEN WEEKDAY(rental_date) IN (5, 6) THEN "weekend"
+        ELSE "workday"
+    END AS DAY_TYPE
+FROM rental;
 
 # 3. You need to ensure that customers can easily access information about the movie collection. To achieve this, retrieve the film titles and their rental duration. If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
 SELECT title, IFNULL(rental_duration, 'Not Available') AS rental_duration
@@ -34,22 +41,27 @@ FROM film
 ORDER BY title ASC;
 
 # 4. Bonus: The marketing team for the movie rental company now needs to create a personalized email campaign for customers. To achieve this, you need to retrieve the concatenated first and last names of customers, along with the first 3 characters of their email address, so that you can address them by their first name and use their email address to send personalized recommendations. The results should be ordered by last name in ascending order to make it easier to use the data.
-# NOT DONE
+SELECT 
+    CONCAT(first_name, " ", last_name) AS full_name, 
+    LEFT(email, 3) AS email_3_characters
+FROM customer
+ORDER BY last_name ASC;
 
 # Challenge 2. 1. Next, you need to analyze the films in the collection to gain some more insights. Using the film table, determine:
 # 1.1 The total number of films that have been released.
-SELECT COUNT(*) AS total_films_released FROM film;
+SELECT COUNT(*) AS total_films_released 
+FROM film;
 
 # 1.2 The number of films for each rating.
-SELECT rating, COUNT(*) AS film_count 
+SELECT rating, COUNT(*) AS number_of_films_by_rating
 FROM film 
 GROUP BY rating;
 
 # 1.3 The number of films for each rating, sorting the results in descending order of the number of films. This will help you to better understand the popularity of different film ratings and adjust purchasing decisions accordingly.
-SELECT rating, COUNT(*) AS film_count 
+SELECT rating, COUNT(*) AS number_of_films_by_rating
 FROM film 
 GROUP BY rating 
-ORDER BY film_count DESC;
+ORDER BY number_of_films_by_rating DESC;
 
 # 2. Using the film table, determine:
 # 2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
@@ -63,3 +75,9 @@ SELECT rating, ROUND(AVG(length), 2) AS avg_duration
 FROM film 
 GROUP BY rating 
 HAVING avg_duration > 120;
+
+# 3. Bonus: determine which last names are not repeated in the table actoSELECT last_name
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING COUNT(last_name) = 1;
